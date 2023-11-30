13929d0ef33daf5b80125add3f38bf81cb699395
zander404 <alexandrearthurpg@gmail.com>
2023-11-30 20:47:51 -0300

    minor changes

--- src/pcd_render/models.py
+++ src/pcd_render/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Form(models.Model):
-    pass
+    choices = [("F","Forte"), ("M","Moderada"), ("L","Leve"), ("N","Nenhuma")]
+    name = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    nausea = models.CharField(max_length=1, choices=choices, name="Nausea")
+    stomach = models.CharField(max_length=1, choices=choices, name="Desconforto Estomacal")
+    discomfort = models.CharField(max_length=1, choices=choices, name="Desconforto")
+    describe = models.TextField(max_length=1000, blank=True, null=True, name="Descrição")
+
+    def __str__(self):
+        return self.name
