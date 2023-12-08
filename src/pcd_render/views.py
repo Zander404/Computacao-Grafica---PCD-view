ace05d0278ab0d039377b4a76a406667cfd5fd2c
zander404 <alexandrearthurpg@gmail.com>
2023-12-08 15:49:31 -0300

    replay image

--- src/pcd_render/views.py
+++ src/pcd_render/views.py
@@ -1,6 +1,29 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+def get_file_count(folder_path):
+    file_count = sum(len(files) for _, _, files in os.walk(folder_path))
+    return file_count
 
 def index(request):
-    return render(request=request, template_name="index1.html")
+    folder_path = os.path.join(settings.BASE_DIR, 'staticfiles/images/andrew9/ply')
+    # Verify that the folder exists before trying to get the file count
+    if os.path.exists(folder_path):
+        frames = get_file_count(folder_path)
+        return render(request, "index1.html", {'frames': frames})
+
+
+def point_cloud1(request):
+    folder_path = os.path.join(settings.BASE_DIR, 'staticfiles/images/andrew9/ply')
+    # Verify that the folder exists before trying to get the file count
+    if os.path.exists(folder_path):
+        frames = get_file_count(folder_path)
+        return render(request, "index1.html", {'frames': frames})
+
+def point_cloud2(request):
+    folder_path = os.path.join(settings.BASE_DIR, 'staticfiles/images/andrew9/ply')
+    # Verify that the folder exists before trying to get the file count
+    if os.path.exists(folder_path):
+        frames = get_file_count(folder_path)
+        return render(request, "index1.html", {'frames': frames})
